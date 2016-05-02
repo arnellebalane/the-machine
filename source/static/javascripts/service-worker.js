a54774b6c19a62964ce3df8a0d3a9b12c03e9d2e
Arnelle Balane <arnellebalane@gmail.com>
2016-05-02 17:23:52 +0800

    display notification when push message is received

---
+++ source/static/javascripts/service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+    console.log('Service Worker Installed.');
+});
+
+
+self.addEventListener('push', function(event) {
+    event.waitUntil(
+        self.registration.showNotification('The Machine', {
+            body: 'We have a new number'
+        })
+    );
+});
