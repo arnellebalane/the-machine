5e785413c807c5d008a9f2dd85a47bff4c8062f0
Arnelle Balane <arnellebalane@gmail.com>
2016-05-02 23:29:25 +0800

    cache necessary paths and respond to fetch events

--- source/static/javascripts/service-worker.js
+++ source/static/javascripts/service-worker.js
@@ -1,3 +1,29 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('the-machine').then(function(cache) {
+            cache.addAll([
+                '/',
+                '/static/stylesheets/main.css',
+                '/static/javascripts/main.js',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(cachedResponse) {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+
 self.addEventListener('push', function(event) {
     event.waitUntil(
         self.registration.showNotification('The Machine', {
