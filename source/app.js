8ef2ab14f4f895ac36bcc4e149d3ed88b34831b6
Arnelle Balane <arnellebalane@gmail.com>
2016-05-02 16:34:07 +0800

    register service worker and subscribe to push notifications

--- source/app.js
+++ source/app.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var nunjucks = require('nunjucks');
 var config = require('./config');
 
@@ -7,7 +8,6 @@ var config = require('./config');
 var app = express();
 
 var TEMPLATES_DIRECTORY = path.join(__dirname, 'templates');
-var STATIC_DIRECTORY = path.join(__dirname, 'static');
 
 app.set('views', TEMPLATES_DIRECTORY);
 nunjucks.configure(TEMPLATES_DIRECTORY, { express: app });
@@ -18,5 +18,15 @@ app.listen(config.get('PORT'), function() {
 });
 
 
+var STATIC_DIRECTORY = path.join(__dirname, 'static');
+var SERVICE_WORKER_PATH = path.join(__dirname, 'static',
+    'javascripts', 'service-worker.js');
+var MANIFEST_PATH = path.join(__dirname, 'manifest.json');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/static', express.static(STATIC_DIRECTORY));
+app.use('/service-worker.js', express.static(SERVICE_WORKER_PATH));
+app.use('/manifest.json', express.static(MANIFEST_PATH));
 app.use('/', require('./routes'));
--- source/routes.js
+++ source/routes.js
@@ -5,6 +5,8 @@ var datastore = require('./lib/datastore');
 var router = express.Router();
 var people = datastore('people');
 
+var subscriptions = [];
+
 
 router.get('/', function(req, res) {
     res.render('index.html');
@@ -21,6 +23,15 @@ router.get('/person/:id', function(req, res) {
     people.query(query).then(function(results) {
         res.render('person.html', { person: results[0] });
     });
+});
+
+
+router.post('/subscribe', function(req, res) {
+    var id = req.body.id;
+    if (subscriptions.indexOf(id) === -1) {
+        subscriptions.push(id);
+    }
+    res.status(200).end();
 });
 
 
