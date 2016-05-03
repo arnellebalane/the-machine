c9ff255d6e2d4a733ad29ab78810887bc4ef9b80
Arnelle Balane <arnellebalane@gmail.com>
2016-05-04 04:55:57 +0800

    fix event name typo in service worker script

--- source/static/javascripts/service-worker.js
+++ source/static/javascripts/service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('install', function(event) {
+self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(keys) {
             return Promise.all(keys.map(function(key) {
